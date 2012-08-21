6b9c7b50c6a48a5a986eda5c7c0db6e25b23b2c7
foxcarlos <foxcarlos@gmail.com>
2012-08-21 16:55:11 -0430

    Se elimina la extension .tar en el nombre del archivofinal para realizar unas pruebas

--- respaldo_pg.py
+++ respaldo_pg.py
@@ -72,7 +72,7 @@ def respaldo_pg():
     nombrearchivo = valores[4][1]
    
     archivofinal = os.path.join(rutarespaldo, nombrearchivo + \
-    nombre_archivo() + '.tar')
+    nombre_archivo() + '')
     comando = 'pg_dump'
 
     '''
