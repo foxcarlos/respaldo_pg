14721c75e8b0c5c1d58d321f283a075777b4b166
foxcarlos <foxcarlos@gmail.com>
2012-08-22 10:17:16 -0430

    cambios no significativos

--- respaldo_pg.py
+++ respaldo_pg.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     print 'Espere un momento ejecutando Proceso de Respaldo...'
     respaldar = respaldo_pg()
     print respaldar
-    #os.system(respaldar)
+    os.system(respaldar)
     print '*** Respaldo Realizado con Exito ***'
 
     
