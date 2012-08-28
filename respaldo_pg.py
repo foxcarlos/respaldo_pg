5fda597c19b97b6656bc00a38189dc0b2d1df43b
foxcarlos <foxcarlos@gmail.com>
2012-08-28 16:03:05 -0430

    Haciendo limpieza de codigo y variables

--- respaldo_pg.py
+++ respaldo_pg.py
@@ -5,17 +5,14 @@ Creado el 09/12/2011
 
 @author: Carlos Garcia
 
-Scripts en python que permite hacer un respaldo de una base de datos  que
-reside en PostgreSql Este archivo se ejecuta de la siguiente manera:
+Scripts en python que permite hacer un respaldo de una base de datos
+que reside en PostgreSql, para ejecutar este archivo es necesario
+que se configure los parametros en el archivo respaldo_pg.conf
+
+Este archivo se ejecuta de la siguiente manera:
 $ python respaldo_postgresql.py
 
-Para Restaurar la Base de Datos:
--d Nombre Base Datos
--U Usuario
-
-pg_restore -d db -U postgres db.tar
 '''
-
 
 import datetime
 import os
@@ -29,38 +26,9 @@ fc = FileConfig(archivo)
 os.environ['PGPASSWORD'] = 'shc21152115'
 
 
-def nombre_archivo():
-    '''
-    Permite Crear el nombre del archivo que tendra el respaldo
-    tomando como nombre la fecha dd-mm-aa y la hora hh:mm:ss
-    Quedando de la siguiente manera suponiendo que fue realizado
-    en la siguiente fecha (24/12/2011 10:30:00) , el resultado seria
-    el siguiente: 24_12_2011_103000
-
-    t = time.localtime()
-    dia = t.tm_mday
-    mes = t.tm_mon
-    ano = t.tm_year
-
-    hora = t.tm_hour
-    minutos = t.tm_min
-    seg = t.tm_sec
-
-    nombre = '%s-%s-%s_%s:%s:%s' % (dia, mes, ano, hora, minutos, seg)
-    '''
-
-    t = datetime.datetime.now()
-    #nombre = t.strftime('%d_%m_%Y_%I%M%S')
-    nombre = t.strftime('%d_')
-
-    return nombre
-
-
 def respaldo_pg():
     '''
-    Toma los parametros del diccionario con el cual se arma el nombre y
-    la ruta que tendra el archivo del respaldo asi como tambien arma
-    el comando final a ejecutar
+     Metodo que permite realizar el respaldo  a una base de datos de postgresql
     '''
 
     valores = fc.opcion_consultar('POSTGRESQL')
@@ -69,21 +37,14 @@ def respaldo_pg():
     nombrebasedatos = valores[1][1]
     usuariobasedatos = valores[2][1]
     rutarespaldo = valores[3][1]
-    #rutarespaldo = os.path.dirname(sys.argv[0])
     nombrearchivo = valores[4][1]
 
-    archivofinal = os.path.join(rutarespaldo, nombrearchivo + nombre_archivo() + '.sql')
+    t = datetime.datetime.now()
+    fecha_dia = t.strftime('%d_')
+
+    archivofinal = os.path.join(rutarespaldo, nombrearchivo + fecha_dia + '.sql')
     comando = 'pg_dump'
 
-    '''
-    -Parametros a pasar(Estos tambien se pudieran colocar dentro del
-    archivo config.cfg),
-    - pero por ahora lo dejaremos asi directo como texto
-    -Ft Significa Format tar
-    -b Inclute campos Blobs
-    -h es el host o la ip del servidor
-    -W password
-    '''
     comando_a_ejecutar = '%s -Fc -b -h %s %s -U %s > "%s"' % \
         (comando, ipservidor, nombrebasedatos, usuariobasedatos, archivofinal)
     return comando_a_ejecutar
@@ -92,5 +53,5 @@ if __name__ == '__main__':
     print 'Espere un momento ejecutando Proceso de Respaldo...'
     respaldar = respaldo_pg()
     print respaldar
-    os.system(respaldar)
+    #os.system(respaldar)
     print '*** Respaldo Realizado con Exito ***'
--- restaura_pg.py
+++ restaura_pg.py
@@ -3,17 +3,21 @@
 '''
 Creado el 23/08/2012
 @author: Carlos Garcia
-
+twitter:@foxcarlos
+email:foxcarlos@gmail.com
 
 Scripts en python que permite restaurar el respaldo de una base de datos
-de PostgreSql Este archivo se ejecuta de la siguiente manera:
+de PostgreSql, al igual que el archivo respaldo_pg.py este tambien depende
+del archuivo de configuracion respaldo_pg.conf el cual contiene inforamcion
+acerca de los parametros necesario para poder realizar el respaldo y restore
+de la Base de Datos
+
+Este archivo se ejecuta de la siguiente manera:
 $ python restaura_pg.py
 
-Para Restaurar la Base de Datos:
--d Nombre Base Datos
--U Usuario
-
-pg_restore -d db -U postgres db.sql
+NOTA:La biblioteca "from rutinas.varias import * debe estar contenida dentro del
+path de python, recuerda que es necesario que la descargues desde el repositorio
+git clone https://github.com/foxcarlos/rutinas.git
 '''
 
 import datetime
@@ -35,19 +39,21 @@ os.environ['PGPASSWORD'] = clave[1]
 
 def restaura(fecha_a_restaurar):
     '''
-    string fecha
+    string numero
+    Fecha a restaurar contiene el numero del dia a restaurar   
     '''
     nombre_archivo = nombrearchivo[1] + fecha_a_restaurar + '_.sql'
     ruta_y_archivo = os.path.join(rutarespaldo[1], nombre_archivo)
-    #pg_restore -i -h localhost -p 5432 -U postgres -d mibase -v "/home/fox/backups/mibase.sql"
 
+    #Primero se procede a eliminar la base de datos del respaldo
     eliminar_bd =   'dropdb  -h %s -p %s -U %s  %s' %\
             (ipservidor[1], puerto[1], usuariobasedatos[1], nombrebasedatos[1])
 
-    #crear_bd = 'createdb -E UTF8 -O admhc -h 10.121.3.31 -p 5434 -U admhc nombrebasedatos'
+    #Se crea nuevamente la base de datos vacia
     crear_bd = 'createdb -O %s -h %s -p %s -U %s %s' %\
             (usuariobasedatos[1], ipservidor[1], puerto[1], usuariobasedatos[1], nombrebasedatos[1])
 
+    #Se restaura el respaldo 
     restaurar_bd = 'pg_restore -i -h %s -p %s -U %s -d %s -v "%s" ' %\
             (ipservidor[1], puerto[1], usuariobasedatos[1], nombrebasedatos[1], ruta_y_archivo)
     
@@ -61,8 +67,7 @@ def restaura(fecha_a_restaurar):
     except:
         print "Ocurrio un error al momento de Restaurar la Base de datos"
 
-
 if __name__ == '__main__':
-    #Day-1 Porque desde restaurar el respaldo del dia anterior
+    #Day-1 Porque see restaura el respaldo del dia anterior
     restaura(str(datetime.date.today().day-1))
     os.system('unset PGPASSWORD')
